5ca930fcfc636ea551b1d85504a1118fa1f2ff98
Mark Cavage <mark.cavage@oracle.com>
2017-08-24 21:17:11 -0700

    return time instead of hello, world

--- src/main/java/com/oracle/crow/Application.java
+++ src/main/java/com/oracle/crow/Application.java
@@ -1,5 +1,12 @@
 package hello;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class Application {
 
     @RequestMapping("/")
-    public String home() {
-        return "Hello Docker World";
+    public Map<String, String> home() {
+        Map<String, String> map = new HashMap<>();
+        TimeZone tz = TimeZone.getTimeZone("UTC");
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+        df.setTimeZone(tz);
+        map.put("time", df.format(new Date()));
+        return map;
     }
 
     public static void main(String[] args) {
